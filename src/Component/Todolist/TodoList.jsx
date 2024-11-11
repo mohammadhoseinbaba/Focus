1b895285384204a88e377c6480ae6bab9997edf0
MRX <sirhb77@gmail.com>
2024-11-11 14:54:32 +0100

    todo list jsx

--- src/Component/Todolist/TodoList.jsx
+++ src/Component/Todolist/TodoList.jsx
@@ -1,6 +1,16 @@
+import './todolist.css'
+
 const TodoList = () => {
   return (
-    <div className=''>img</div>
+    <div className='Todolist'>
+      <div className="up">
+        <p>Tasks</p>
+        <img src="./more.png" alt="" />
+      </div>
+      <div className="down">
+        <button className="Addtask">add task</button>
+      </div>
+    </div>
   )
 }
 
