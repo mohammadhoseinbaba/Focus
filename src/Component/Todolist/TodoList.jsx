6f3bc39a81bfc13bce4130d974c732ce31e95031
MRX <sirhb77@gmail.com>
2024-11-12 12:02:52 +0100

    todolist logic

--- src/Component/Todolist/TodoList.jsx
+++ src/Component/Todolist/TodoList.jsx
@@ -1,8 +1,32 @@
+import { useState } from 'react'
 import './todolist.css'
 
 const TodoList = () => {
+  const [whatTodo, SetWhatTodo] = useState('')
+  const [estimateNumber, setEstimateNumber] = useState('')
+
+  const handleSubmit = () => {
+    let li = document.createElement('li');
+    let T = document.createTextNode(whatTodo)
+    li.appendChild(T);
+    if (whatTodo === '') {
+      alert('Enter your text')
+    } else {
+      document.getElementById('myul').appendChild(li)
+    }
+    SetWhatTodo('')
+  }
+
+  const handleChange = (event) => {
+    SetWhatTodo(event.target.value)
+  }
+
+  const handleForm = (e) => {
+    e.preventDefault()
+  }
+
   const handleClick = () => {
-    let box=document.getElementById('box')
+    let box = document.getElementById('box')
     box.classList.toggle('yesdisplay')
   }
 
@@ -14,21 +38,23 @@ const TodoList = () => {
       </div>
       <div className="middle">
         <div className="result">
-          <p>the job to do</p>
-          <div className='right'><p>0/4</p>
+          <ul id='myul'>
+
+          </ul>
+          <div className='right'><p>0/{estimateNumber}</p>
             <img src="moregray.png" alt="" />
           </div>
 
         </div>
-          <div className="box" id='box'>
-            <form>
-            <input  className='first' type="text" placeholder='what are you working on?' />
+        <div className="box" id='box'>
+          <form onClick={handleForm}>
+            <input className='first' type="text" placeholder='what are you working on?' onChange={handleChange} value={whatTodo} />
             <label htmlFor="">Estimated Pomodoro</label>
-            <input type="number" placeholder='Number' />  
+            <input type="number" placeholder='Number' onChange={(e) => { setEstimateNumber(e.target.value) }} value={estimateNumber} />
             <div className='buttons'>
-            <button className='cancel'>cancel</button>
-            <button>save</button>
-          </div>
+              <button className='cancel'>cancel</button>
+              <button onClick={handleSubmit}>save</button>
+            </div>
           </form>
         </div>
 
