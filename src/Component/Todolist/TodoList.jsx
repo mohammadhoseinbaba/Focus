42f04d7eb82c37a8571e4bfd248606403d7ab1b2
MRX <sirhb77@gmail.com>
2024-11-12 17:10:27 +0100

    fixing issue

--- src/Component/Todolist/TodoList.jsx
+++ src/Component/Todolist/TodoList.jsx
@@ -1,26 +1,31 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import './todolist.css'
 
 const TodoList = () => {
   const [whatTodo, SetWhatTodo] = useState('')
   const [estimateNumber, setEstimateNumber] = useState('')
-const [isShown,setIsShown]=useState(true)
+  const [isShown, setIsShown] = useState(true)
+  const [task, SetTask] = useState([])
 
-  const handleSubmit = () => {
-
-  }
-
-  const handleChange = (event) => {
-    SetWhatTodo(event.target.value)
-  }
 
   const handleForm = (e) => {
     e.preventDefault()
+
+    if(!whatTodo.trim() || !estimateNumber.trim()){
+      alert('Wrong information please recheck it')
+      return
+     }
+    
+    const newtask = { text: whatTodo, estimate: estimateNumber }
+    SetTask([...task, newtask])
+    setEstimateNumber('')
+    SetWhatTodo('')
+
+    setIsShown(!isShown)
+
   }
 
-  const handleClick = () => {
-    setIsShown(!isShown)
-    }
+ 
 
   return (
     <div className='Todolist'>
@@ -31,24 +36,27 @@ const [isShown,setIsShown]=useState(true)
       <div className="middle">
         <div className="result">
           <ul id='myul'>
-
+            {task.map((task, index) => (
+              <li key={index}>
+                {task.text} <span>{task.estimate}</span>
+              </li>
+            ))}
           </ul>
         </div>
-        <div className={isShown? 'box': 'box yesdisplay'} id='box'>
-          <form onClick={handleForm}>
-            <input className='first' type="text" placeholder='what are you working on?' onChange={handleChange} value={whatTodo} />
-            <label htmlFor="">Estimated Pomodoro</label>
+        <div className={isShown ? 'box' : 'box yesdisplay'} id='box'>
+          <form onSubmit={handleForm}>
+            <input className='first' type="text" placeholder='what are you working on?' onChange={e=>SetWhatTodo(e.target.value)} value={whatTodo} />
+            <label htmlFor="estimate">Estimated Pomodoro</label>
             <input type="number" placeholder='Number' onChange={(e) => { setEstimateNumber(e.target.value) }} value={estimateNumber} />
             <div className='buttons'>
-              <button className='cancel'>cancel</button>
-              <button onClick={handleSubmit}>save</button>
+              <button type='button' className='cancel' onClick={() => { setIsShown(true) }}>cancel</button>
+              <button type='submit' >save</button>
             </div>
           </form>
         </div>
-
       </div>
       <div className="down">
-        <button className="Addtask" onClick={handleClick}>add task</button>
+        <button className="Addtask" onClick={e=>{setIsShown(!isShown)}}>add task</button>
       </div>
     </div>
   )
