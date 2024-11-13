f04a3bdf82e59befd6ee55c1b267dd06caa877e2
MRX <sirhb77@gmail.com>
2024-11-13 21:25:09 +0100

    updated

--- src/Component/Focus/Focus.jsx
+++ src/Component/Focus/Focus.jsx
@@ -2,23 +2,56 @@ import { useEffect, useState } from 'react'
 import './focus.css'
 
 const Focus = () => {
-  const [isStart, setIsStart] = useState(false)
 
-  const handleStart = () => {
-    setIsStart(!isStart)
+
+
+  const Timer = () => {
+    const [timeLeft, setTimeLeft] = useState(25 * 60)
+    const [isRunning, setIsRunning] = useState(false)
+    const [timerMode, setTimerMode] = useState('pomodoro')
+    useEffect(() => {
+      let interval;
+      if (isRunning) {
+        interval = setInterval(() => {
+          setTimeLeft((prevTime) => {
+            if (prevTime <= 0) {
+              clearInterval(interval) //clear the interval when the time is reached up
+              return 0;
+            }
+            return prevTime - 1 //decrement the time by one second
+          })
+        }, 1000);
+      }
+      return () => clearInterval(interval)
+    }, [isRunning])
+
+    const formatTime = (time) => {
+      const minutes = Math.floor(time / 60)
+      const seconds = time % 60
+      return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+    }
+
+    return <div>{formatTime(timeLeft)}</div>
   }
+
+  const handlePomodoro = () => {
+    setTimerMode('pomodoro')
+    setTimeLeft(25*60)
+    setIsRunning(false)
+      }
+      
 
   return (
     <div className='Focus'>
       <div className="mainpart">
         <div className="buttons">
-          <button>Pomodoro</button>
+          <button type='button' onClick={handlePomodoro}>Pomodoro</button>
           <button>Short Break</button>
           <button>Long Break</button>
         </div>
-        <div className="timer">25:00</div>
-        <div className="start" onClick={handleStart}>
-          <button className='buttonmain'>{isStart ? 'Stop' : 'Start'}</button>
+        <div className="timer"><Timer /></div>
+        <div className="contorols" >
+          {!isRunning ? (<button className='buttonmain' onClick={() => { setIsRunning(true) }}>Start</button>) : (<button className='buttonmain' onClick={() => { setIsRunning(false) }}>Stop</button>)}
         </div>
       </div>
       <div className="counter">#1</div>
